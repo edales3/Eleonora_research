a992bc9251ebb5bcc1bad6dfa390449ed9307107
edales3 <54680786+edales3@users.noreply.github.com>
2020-03-10 12:45:15 -0500

    Add files via upload

--- tabular_maxent_irl/tabular_validation.py
+++ tabular_maxent_irl/tabular_validation.py
@@ -5,27 +5,19 @@ from q_iteration import q_iteration, logsumexp, get_policy
 from maxent_irl import tabular_maxent_irl, compute_visitation
 from simple_env import random_env
 
-def direct_method(env,true_sa_visits,discount,dim_obs,reward,transition,state_only):
+def direct_method(env,ent_wt,true_sa_visits,discount,dim_obs,reward,transition,state_only):
 # this version of the direct method solves a system of linear equations (as in slide 12)
     learned_rew, learned_q = tabular_maxent_irl(env, true_sa_visits, lr=0.01, num_itrs=1000,
-                                                ent_wt=1.0, state_only=state_only,
+                                                ent_wt=ent_wt, state_only=state_only,
                                                 discount=discount)
-    learned_policy = get_policy(learned_q, ent_wt=1.0)
+    learned_policy = get_policy(learned_q, ent_wt=ent_wt)
     policy_irl = np.argmax(learned_policy, axis=1)
     reward_array_irl = np.array([reward[k, policy_irl[k]] for k in range(dim_obs)])
     transition_matrix = np.array([transition[j, policy_irl[j]] for j in range(dim_obs)])
 
-    eps = 10e-9
-    V_irl = np.zeros(dim_obs)
-    delta = 2 * eps
-
-    while delta > eps:
-        delta_max = -1
-        v_prev = copy.deepcopy(V_irl)
-        V_irl = reward_array_irl + discount * np.dot(transition_matrix, V_irl)
-        if abs(V_irl - v_prev).any() > delta_max:
-            delta_max = np.max(abs(V_irl - v_prev))
-        delta = delta_max
+    A = np.identity(dim_obs) - discount*transition_matrix
+    b = reward_array_irl
+    V_irl = np.linalg.solve(A,b)
 
     return V_irl
 
@@ -53,7 +45,7 @@ if __name__ == "__main__":
 
     # Creating the environment using "simple_env.py", collecting true reward and transition probabilities
     env = random_env(10, 5, seed=1, terminate=False, t_sparsity=0.8)
-    ent_wt = 1.0
+    ent_wt = 0.1
     discount = 0.9
     dim_obs = env.observation_space.flat_dim
     dim_act = env.action_space.flat_dim
@@ -66,6 +58,6 @@ if __name__ == "__main__":
 
     # Computing V_irl (state-only depencency) and V_irl_sa (state-action dependency) using the direct method
     # and V_star using value iteration
-    V_irl = direct_method(env,true_sa_visits,discount,dim_obs,reward,transition,state_only=True)
-    V_irl_sa = direct_method(env,true_sa_visits,discount,dim_obs,reward,transition,state_only=False)
+    V_irl = direct_method(env,ent_wt,true_sa_visits,discount,dim_obs,reward,transition,state_only=True)
+    V_irl_sa = direct_method(env,ent_wt,true_sa_visits,discount,dim_obs,reward,transition,state_only=False)
     V_star = value_iteration(dim_obs,reward,discount,transition)
